5f4d76c84e746c5865d2f4e12dab83794a2eb060
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-10-05 17:17:33 +0530

    5th oct 2012

--- src/com/example/game/FriendList.java
+++ src/com/example/game/FriendList.java
@@ -12,20 +12,28 @@ import org.json.JSONObject;
 import com.facebook.android.AsyncFacebookRunner;
 import com.facebook.android.Facebook;
 import com.facebook.android.FacebookError;
+import com.facebook.android.R;
 import com.facebook.android.Util;
 import com.facebook.android.AsyncFacebookRunner.RequestListener;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class FriendList extends Activity {
-	private ArrayList<Friend> friendArray;
+	//private ArrayList<Friend> friendArray;
+	public static JSONArray jsonArray;
 	private Facebook facebook = new Facebook(Utils.APP_ID);
 	ListView lv;
     private AsyncFacebookRunner mAsyncRunner = new AsyncFacebookRunner(facebook);
-    FriendAdapter mFriendAdapter;
+    FriendListAdapter mFriendAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,34 +50,13 @@ public class FriendList extends Activity {
 		@Override
 		public void onComplete(String response, Object state) {
 			JSONObject json;
-			Log.d("name", "In Litsner On complete"+ response);
+			//Log.d("name", "In Litsner On complete"+ response);
 			try {
 				json = Util.parseJson(response);
-				final JSONArray friends = json.getJSONArray("data");
-				
-				friendArray = new ArrayList<Friend>();
-				for (int i = 0; i < friends.length(); i++) {
-					
-					JSONObject friend = friends.getJSONObject(i);
-					Friend frnd = new Friend();
-					
-					frnd.setName(friend.getString("name"));
-					frnd.setId(i+1);
-					frnd.setFbId(friend.getString("id"));
-					friendArray.add(frnd);
-					Log.d("name", friend.getString("name"));
-				}
-				runOnUiThread(new Runnable() {
-					
-					@Override
-					public void run() {
-						lv = (ListView)findViewById(R.id.friend_list);
-						mFriendAdapter = new FriendAdapter(FriendList.this, friendArray);
-						lv.setAdapter(mFriendAdapter);
-						
-						
-					}
-				});
+				jsonArray = json.getJSONArray("data");
+				Log.d("name", "In Litsner On complete"+ jsonArray.toString());
+				lv = (ListView)findViewById(R.id.friend_list);
+				mFriendAdapter = new FriendListAdapter(FriendList.this);
 			} catch (FacebookError e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -109,5 +96,77 @@ public class FriendList extends Activity {
 		
 		
 	}
+    
+    public class FriendListAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+        FriendList friendsList;
+        JSONArray json_array;
+
+        public FriendListAdapter(FriendList friendsList) {
+            this.friendsList = friendsList;
+            if (Utility.model == null) {
+                Utility.model = new FriendsGetProfilePics();
+            }
+            Utility.model.setListener(this);
+            mInflater = LayoutInflater.from(friendsList.getBaseContext());
+        }
+
+        @Override
+        public int getCount() {
+            Log.d("name", ""+jsonArray.length());
+        	return jsonArray.length();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = jsonArray.getJSONObject(position);
+            } catch (JSONException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            View hView = convertView;
+            if (convertView == null) {
+                hView = mInflater.inflate(R.layout.grid_item, null);
+                ViewHolder holder = new ViewHolder();
+                holder.name = (TextView) hView.findViewById(R.id.name);
+                holder.icon = (ImageView) hView.findViewById(R.id.icon);
+                hView.setTag(holder);
+            }
+
+            ViewHolder holder = (ViewHolder) hView.getTag();
+            try {
+                    holder.icon.setImageBitmap(Utility.model.getImage(jsonObject.getString("id"), jsonObject.getString("picture")));
+                
+            } catch (JSONException e) {
+                holder.name.setText("");
+            }
+            try {
+                holder.name.setText(jsonObject.getString("name"));
+            } catch (JSONException e) {
+                holder.name.setText("");
+            }
+           
+            return hView;
+        }
+
+    }
+
+    class ViewHolder {
+        ImageView icon;
+        TextView name;
+        
+    }
     
 }
